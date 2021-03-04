932ecc925210b9a9d985231aeda14ac5638d13b9
Akosnikowski <78250440+Akosnikowski@users.noreply.github.com>
2021-03-04 15:10:32 -0800

    Initial commit

---
+++ app/src/main/java/com/android/example/housingconnect/ImageUploadResponse.kt
@@ -0,0 +1,5 @@
+package com.android.example.housingconnect
+
+// TODO: PHASE 2.2 - Define ImageUploadResponse Data Class
+//  Stores two variables of type String called message and type
+data class ImageUploadResponse(var message: String, var type: String)
---
+++ app/src/main/java/com/android/example/housingconnect/Message.kt
@@ -0,0 +1,5 @@
+package com.android.example.housingconnect
+
+// TODO: PHASE 2.2 - Define Message Data Class
+//  Stores a variable of type String called message
+data class Message(var message: String)
